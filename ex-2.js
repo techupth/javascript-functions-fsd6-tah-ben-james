8886637961c726c868fbef7e2451212e3930d4cd
Ben Wattanasup <benwata777@gmail.com>
2024-05-14 13:52:06 +0700

    java script

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,23 @@
 // Start coding here
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+const multiply = function (a, b) {
+  return a * b;
+};
+const divide = function (a, b) {
+  return a / b;
+};
+
+let result1 = add(10, 5);
+console.log(result1);
+let result2 = subtract(10, 5);
+console.log(result2);
+let result3 = multiply(10, 5);
+console.log(result3);
+let result4 = divide(10, 5);
+console.log(result4);

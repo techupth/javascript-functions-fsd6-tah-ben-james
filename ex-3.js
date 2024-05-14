765459227860171b9dfe724fd96315ed8bafe3e1
James <apisitamornktn@gmail.com>
2024-05-14 12:31:38 +0700

    feat:JavaScript Functions Assignment

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,24 @@
 // Start coding here
+let add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(10, 5));
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+console.log(subtract(10, 5));
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(multiply(10, 5));
+
+let divide = (a, b) => {
+  return a / b;
+};
+
+console.log(divide(10, 5));
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,22 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  subtract: (a, b) => {
+    return a - b;
+  },
+
+  multiply: (a, b) => {
+    return a * b;
+  },
+
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+let addResult = calculator.add(10, 20);
+
+console.log(addResult);
